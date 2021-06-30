f477d3866ba9145913935cec4ce328ab81d66fc0
Gabriel <gabriel.gabrielyea@gmail.com>
2021-06-29 20:36:55 -0500

    Add link to about page

--- scripts/main.js
+++ scripts/main.js
@@ -4,8 +4,12 @@ import MobileMenu from './mobileMenu.js';
 const sFunctions = new SpeakerFunctions();
 const mobileMenu = new MobileMenu();
 
-// init? dont know how to avoid the linter error I just need to set the references
-// If I use the constructor only I get linter error
-mobileMenu.init();
+window.addEventListener('load', () => {
+  // init? dont know how to avoid the linter error I just need to set the references
+  // If I use the constructor only I get linter error
+  mobileMenu.init();
 
-sFunctions.setSpeakerData();
+  if (sFunctions.templateContainer != null) {
+    sFunctions.setSpeakerData();
+  }
+});
