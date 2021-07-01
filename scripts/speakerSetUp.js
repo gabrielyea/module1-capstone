f5323cdccbde0cd5380aa0ce21f69792f614b904
Gabriel <gabriel.gabrielyea@gmail.com>
2021-06-30 19:35:32 -0500

    Add show more / show less btn to speaker section

--- scripts/speakerSetUp.js
+++ scripts/speakerSetUp.js
@@ -40,7 +40,16 @@ export default class SpeakerFunctions {
     });
   };
 
+  // index is the default number of speakers that should be always on display
+  // also set the message inside this function just because it was small
   toggleSpeakerFrom = (index) => {
+    const setMessage = (msg) => {
+      this.toggleBtn.innerHTML = msg;
+    };
+    // probably a much better way to do this but got a little bit lazy
+    const btnMsg = this.toggleBtn.innerText;
+    if (btnMsg === 'Show more' ? setMessage('Show less') : setMessage('Show more'));
+
     for (let i = index; i < this.speakerList.length; i += 1) {
       this.speakerList[i].classList.toggle('show');
     }
